0ca909d52869491752c1d66c54fbe079735ddd4e
Shumaila Riaz <shumailariaz215@gmail.com>
2026-02-11 14:30:07 +0500

    middleware added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const connectDB = require("./db/connect");
 
 const app = express();
 const port = 5000;
+
 app.use(express.json());
-const authRoutes = require("./routes/auth");
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
 connectDB();
+
+const authRoutes = require("./routes/auth");
 const expenseRoutes = require("./routes/expense");
-app.use("/api/v1/expenses", expenseRoutes);
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/expenses", expenseRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
